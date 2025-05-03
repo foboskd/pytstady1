8bbaf17bf07a462740568260a320f786116a8e5e
Кирилл Двуреченский <foboskd@gmail.com>
2025-05-03 14:49:27 +0300

    homework №2 task 1

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ for _ in range(3):
     date = input("Введите дату: ")
     task = input("Введите задачу: ")
     tasks[date] = task
+
+exit = input("Введите команду: ")
+
+if exit == 'exit':
+    print("Спасибо за использование! До свидания!")
