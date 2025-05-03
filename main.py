7100212891f7dff5ae60fed5bb608abdc24f4d56
Кирилл Двуреченский <foboskd@gmail.com>
2025-05-03 12:41:36 +0300

    3 step

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-tasks = []
+tasks = {}
 
 for _ in range(3):
     date = input("Введите дату: ")
     task = input("Введите задачу: ")
-    tasks.append(f"{date} {task}")
-
-for entry in tasks:
-    print(entry)
