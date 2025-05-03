29b104ac0a3b76d417c866311fb0abad7821818f
Кирилл Двуреченский <foboskd@gmail.com>
2025-05-03 12:36:36 +0300

    2 step hometask

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-date = input("Введите дату: ")
-task = input("Введите задачу: ")
-print(date, task)
+tasks = []
+
+for _ in range(3):
+    date = input("Введите дату: ")
+    task = input("Введите задачу: ")
+    tasks.append(f"{date} {task}")
+
+for entry in tasks:
+    print(entry)
